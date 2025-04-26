4e984ae58504966855820c0d124d6f22e783cf01
rajeswari-Machina <rajeswarimachina02@gmail.com>
2025-04-26 06:46:05 +0530

    updated cors policy for deployed frontend

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(rateLimit({
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173','https://feedbackcollector1.netlify.app/'] 
 }));
 app.use('/api/', feedbackRoutes);
 app.use('/api/auth',authRoute);
